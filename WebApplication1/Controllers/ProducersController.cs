76eed8b5d0e71db677b90c065151dd8e86b15dec
firas1996 <firas.hm.1996@gmail.com>
2023-04-14 14:46:08 +0200

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Controllers/ProducersController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.Cinema;
+
+namespace WebApplication1.Controllers
+{
+    public class ProducersController : Controller
+    {
+        CinemaDBContext ctx;
+        public ProducersController(CinemaDBContext context)
+        {
+            ctx= context;
+        }
+        // GET: ProducersController
+        public ActionResult Index()
+        {
+            return View(ctx.Producers.ToList());
+        }
+
+        // GET: ProducersController/Details/5
+        public ActionResult Details(int id)
+        {
+            Producer p = ctx.Producers.Find(id);
+
+            return View(p);
+        }
+
+        // GET: ProducersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProducersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Producer prod)
+        {
+            try
+            {
+                ctx.Producers.Add(prod);
+                ctx.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProducersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Producer p = ctx.Producers.Find(id);
+            return View(p);
+        }
+
+        // POST: ProducersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection,Producer prod)
+        {
+            try
+            {
+                ctx.Producers.Update(prod);
+                ctx.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProducersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Producer p = ctx.Producers.Find(id);
+
+            return View(p);
+        }
+
+        // POST: ProducersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection,Producer prod)
+        {
+            try
+            {
+                ctx.Producers.Remove(prod);
+                ctx.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
